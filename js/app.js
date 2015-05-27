65d37e082a01c0ec31ae54bda6e598f0a8762b2f
Marc Christie <mchristie@walkerinfo.com>
2015-05-27 09:50:41 -0400

    Integrated JSDoc and added a bit of JSDoc to app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 /*global window, Ember, DS, Todos:true */
 window.Todos = Ember.Application.create();
 
+/**
+ * JSDoc test
+ * @class
+ */
 Todos.ApplicationAdapter = DS.LSAdapter.extend({
 	namespace: 'todos-emberjs'
 });
